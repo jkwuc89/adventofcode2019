2d5249259e2b9e5282bcdfc628519443cd2d7dc3
Keith Wedinger <keith.wedinger@joinroot.com>
2019-12-06 07:10:35 -0500

    Refactored day 2 puzzle 1 solution to better support day 2 puzzle 2

--- lib/day_two.rb
+++ lib/day_two.rb
@@ -5,31 +5,25 @@ class DayTwo
   MULTIPLE_OP_CODE = 2
   END_PROGRAM_OP_CODE = 99
 
-  def parse_program(program:)
-    program.split(",").map(&:to_i)
-  end
+  def run_program(program:, noun: nil, verb: nil)
+    parsed_and_executed_program = program.split(",").map(&:to_i)
 
-  def run_program(program:, is_running_puzzle_input: false)
-    parsed_and_executed_program = parse_program(:program => program)
-
-    if is_running_puzzle_input
-      parsed_and_executed_program[1] = 12
-      parsed_and_executed_program[2] = 2
-    end
+    parsed_and_executed_program[1] = noun if noun
+    parsed_and_executed_program[2] = verb if verb
 
     op_code_position = 0
     loop do
       op_code = parsed_and_executed_program[op_code_position]
       break if op_code == END_PROGRAM_OP_CODE
 
-      input_1_position = parsed_and_executed_program[op_code_position + 1]
-      input_2_position = parsed_and_executed_program[op_code_position + 2]
+      parameter_1_position = parsed_and_executed_program[op_code_position + 1]
+      parameter_2_position = parsed_and_executed_program[op_code_position + 2]
       output_position = parsed_and_executed_program[op_code_position + 3]
 
-      parsed_and_executed_program[output_position] = _execute_operation(
+      parsed_and_executed_program[output_position] = _execute_instruction(
         :op_code => op_code,
-        :input_1 => parsed_and_executed_program[input_1_position],
-        :input_2 => parsed_and_executed_program[input_2_position]
+        :parameter_1 => parsed_and_executed_program[parameter_1_position],
+        :parameter_2 => parsed_and_executed_program[parameter_2_position]
       )
 
       op_code_position += 4
@@ -40,12 +34,12 @@ class DayTwo
 
   private
 
-  def _execute_operation(op_code:, input_1:, input_2:)
+  def _execute_instruction(op_code:, parameter_1:, parameter_2:)
     case op_code
     when ADD_OP_CODE
-      result = input_1 + input_2
+      result = parameter_1 + parameter_2
     when MULTIPLE_OP_CODE
-      result = input_1 * input_2
+      result = parameter_1 * parameter_2
     else
       raise InvalidOpCodeError
     end
--- spec/day_two_spec.rb
+++ spec/day_two_spec.rb
@@ -4,20 +4,6 @@ require 'utils'
 describe DayTwo do
   let(:day_two) { described_class.new }
 
-  context "#parse_program" do
-    context "example input" do
-      it { expect(day_two.parse_program(:program => "1,9,10,3,2,3,11,0,99,30,40,50")).to eq([1,9,10,3,2,3,11,0,99,30,40,50]) }
-    end
-
-    context "puzzle input" do
-      let(:program) { File.read("input/day_two.txt") }
-
-      it { expect(day_two.parse_program(:program => program)).to eq([
-        1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,10,23,2,13,23,27,1,5,27,31,2,6,31,35,1,6,35,39,2,39,9,43,1,5,43,47,1,13,47,51,1,10,51,55,2,55,10,59,2,10,59,63,1,9,63,67,2,67,13,71,1,71,6,75,2,6,75,79,1,5,79,83,2,83,9,87,1,6,87,91,2,91,6,95,1,95,6,99,2,99,13,103,1,6,103,107,1,2,107,111,1,111,9,0,99,2,14,0,0
-      ]) }
-    end
-  end
-
   context "#run_program" do
     let(:result) { day_two.run_program(:program => program) }
 
@@ -77,7 +63,7 @@ describe DayTwo do
 
     context "with puzzle input" do
       let(:program) { File.read("input/day_two.txt") }
-      let(:result) { day_two.run_program(:program => program, :is_running_puzzle_input => true) }
+      let(:result) { day_two.run_program(:program => program, :noun => 12, :verb => 2) }
 
       it { expect(result[0]).to eq(4138687) }
     end
